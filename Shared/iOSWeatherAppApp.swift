ec2413d126e30abb4218873ba72c55a9460aa4ae
Jan Ślusarz <jan.sluarz@gmail.com>
2021-05-08 14:00:07 +0200

    init

---
+++ Shared/iOSWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSWeatherAppApp.swift
+//  Shared
+//
+//  Created by Jan Ślusarz on 08/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct iOSWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
